6be74683c9b74fab7017b2a925a44a072564a784
Catherine Parnell <catherine.d.parnell.22@dartmouth.edu>
2020-12-05 20:21:19 -0600

    firebase implemented

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDBgFH0trwdSnwKq30Lj4H2EfDu8xCsGKU",
+    authDomain: "chrome-duke.firebaseapp.com",
+    projectId: "chrome-duke",
+    storageBucket: "chrome-duke.appspot.com",
+    messagingSenderId: "919440526614",
+    appId: "1:919440526614:web:2d21458b94ff325b951437"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
         console.log(tab.url);
@@ -27,6 +41,25 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
                 } else if (greenCheck == false) {
                     console.log("GreenCheck: ", greenCheck);
                     chrome.browserAction.setIcon({path:'images/red.png', tabId: activeInfo.tabId}); 
+                }
+                // this would be a username from authentication if authenticated
+                let user = "user";
+                if (user) {
+                    db.collection("website").add({
+                        website: strippedUrl,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                        green: greenCheck,
+                    })
+                    .then(function(docRef) {
+                        console.log("Document written in ID: ", docRef.id);
+                        // this puts the made website reference in an array of websites attached to the user
+                        db.collection("users").doc(user).update({
+                            websites: firebase.firestore.FieldValue.arrayUnion(docRef.id)
+                        })
+                    })
+                    .catch(function(error) {
+                        console.error("Error adding document: ", error);
+                    });
                 }
             });
         }).catch(error => {
